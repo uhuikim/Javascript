7a713d9ee381cd775162d3c0fc6e67aa7005541e
uhuikim <uhui94@naver.com>
2021-07-07 12:13:58 +0900

    feat: 3-6 라우터 처리

--- kimmintae/app.js
+++ kimmintae/app.js
@@ -10,31 +10,47 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-
 const ul = document.createElement("ul");
 
-window.addEventListener("hashchange", function () {
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+
+  const newsList = [];
+  newsList.push("<ul>");
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+    <li>
+      <a href=#${newsFeed[i].id}>${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
+    </li>
+    `);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
   const id = location.hash.substr(1);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement("h1");
-  title.innerHTML = newsContent.title;
 
-  content.appendChild(title);
-});
-
-for (let i = 0; i < newsFeed.length; i++) {
-  const div = document.createElement("div");
-
-  div.innerHTML = `
-  <li>
-    <a href=#${newsFeed[i].id}>${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
-  </li>
+  container.innerHTML = `
+  <h1>${newsContent.title}</h1>
+  <div><a href="#">목록으로</a></div>
   `;
-
-  ul.appendChild(div.firstElementChild);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
